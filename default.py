148fa2f13b98571f1e41b3e8cc41917dafdc9af0
b-jesch <birger.jesch@gmail.com>
2015-01-14 10:56:13 +0100

    add additional files

--- default.py
+++ default.py
@@ -1,33 +1,74 @@
-# das vollständige Skript
-
+import os
+import re
 import xbmc, xbmcgui, xbmcaddon
 
-addon = xbmcaddon.Addon("service.playback.sleeptimer")
-counter = 180   # 3 Minuten (180 sec)
-bar = 0         # Progressanzeige (Null Prozent)
+__addon__ = xbmcaddon.Addon()
+__addonname__ = __addon__.getAddonInfo('id')
+__path__ = __addon__.getAddonInfo('path')
+__version__ = __addon__.getAddonInfo('version')
+__LS__ = __addon__.getLocalizedString
 
-while(not xbmc.abortRequested):
-    # Testen ob Idletimer länger als Idlezeit idlet, 180 sec abziehen
-    if xbmc.getGlobalIdleTime() > (int(addon.getSetting('idle_time_min'))*60 - counter):
+__iconDefault__ = xbmc.translatePath(os.path.join(__path__, 'resources', 'media', 'pawprint.png'))
+
+def notifyLog(message, level=xbmc.LOGNOTICE):
+    xbmc.log('%s: %s' % (__addonname__, message.encode('utf-8')), level)
+
+class SleepyWatchdog(object):
+
+    def __init__(self):
+        self.maxIdleTime = None
+        self.action = None
+        self.showPopup = None
+        self.notificationTime = None
+        self.PopUp = xbmcgui.DialogProgress()
+        self.Player = xbmc.Player()
+
+        self.getWDSettings()
+
+    def getWDSettings(self):
+        self.maxIdleTime = int(__addon__.getSetting('maxIdleTime'))
+        self.action = __addon__.getSetting('action')
+        self.notifyUser = True if __addon__.getSetting('showPopup').upper() == 'TRUE' else False
+        self.notificationTime = int(re.match('\d+', __addon__.getSetting('notificationTime')).group())
+
+    # user defined actions
+
+    def stopVideoAudioTV(self):
+        if self.Player.isPlaying():
+            self.Player.stop()
 
 
-        # Popup starten
-        PopUp = xbmcgui.DialogProgress()
-        PopUp.create('Inaktivitätstimer', 'laufendes Video wird in %s Sekunden gestoppt.' % counter)
-        PopUp.update(bar)
-        # Progressbar synchronisieren, 180 sec runterzählen
-        while bar < counter and not PopUp.iscanceled():
-            bar += 1
-            percent = int(bar * 100 / counter)
-            PopUp.update(percent, 'laufendes Video wird in %s Sekunden gestoppt.' % (counter - bar))
-            xbmc.sleep(1000)
+    def start(self):
+        while(not xbmc.abortRequested):
 
-        # Popup schliessen
-        PopUp.close()
-        if not PopUp.iscanceled():
-            # definierte Aktionen ausführen
-            # Player stoppen
-            if xbmc.Player().isPlaying():
-                xbmc.Player().stop()
-                
-    xbmc.sleep(60000)
+            # Check if GlobalIdle longer than maxIdle
+            if xbmc.getGlobalIdleTime() > (self.maxIdleTime - int(self.notifyUser)*self.notificationTime):
+                # Check if notification is allowed
+                if self.notifyUser:
+                    _bar = 0
+                    self.PopUp.create(__LS__(32100), 'laufendes Video wird in %s Sekunden gestoppt.' % self.notificationTime)
+                    self.Popup.update(_bar)
+                # synchronize progressbar
+                while _bar < self.notificationTime and not self.PopUp.iscanceled():
+                    _bar += 1
+                    _percent = int(_bar * 100 / self.notificationTime)
+                    self.PopUp.update(_percent, 'laufendes Video wird in %s Sekunden gestoppt.' % (self.notificationTime - _bar))
+                    xbmc.sleep(1000)
+
+                # Popup schliessen
+                self.PopUp.close()
+                if not self.Popup.iscanceled():
+                    pass
+                    #
+                    # ToDo: implement user defined actions here
+                    #
+
+            xbmc.sleep(60000)
+
+# MAIN #
+try:
+    WatchDog = SleepyWatchdog()
+    WatchDog.start()
+except Exception, e:
+    notifyLog('%s' %e, xbmc.LOGERROR)
+del WatchDog
