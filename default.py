25a21febde9316d49c3d6110ef6065532a91ae46
Birger Jesch <birger.jesch@gmail.com>
2019-08-28 11:39:42 +0200

    some improvements

--- default.py
+++ default.py
@@ -3,18 +3,32 @@ if __name__ == '__main__':
     from service import *
 
     dialog = xbmcgui.Dialog()
-    _idx = dialog.select(LOC(32119),[LOC(32180), LOC(32181), LOC(32182), LOC(32183), LOC(32184), LOC(32185),
-                                     LOC(32186), LOC(32187), LOC(32188), LOC(32189)])
-    if _idx > -1:
-        _userIdleTime = LOC(32180 + _idx)
+    _offset = 1
 
-        mode = 'USER'
+    if ADDON.getSetting('mode') == 'USER':
+        _idx = dialog.select(LOC(32119), [LOC(32210), LOC(32180), LOC(32181), LOC(32182), LOC(32183), LOC(32184),
+                                          LOC(32185), LOC(32186), LOC(32187), LOC(32188), LOC(32189)])
+    else:
+        _idx = dialog.select(LOC(32119), [LOC(32180), LOC(32181), LOC(32182), LOC(32183), LOC(32184), LOC(32185),
+                                          LOC(32186), LOC(32187), LOC(32188), LOC(32189)])
+        _offset = 0
+
+    if _idx > -1:
+        _userIdleTime = LOC(32180 + _idx + _offset)
+
+        if _offset > 0 and _idx == 0:
+            mode = 'SERVICE'
+        else:
+            mode = 'USER'
 
         ADDON.setSetting('mode', mode)
-        ADDON.setSetting('userIdleTime', str(int((re.match('\d+', _userIdleTime).group())) * 60))
-
-        _action = int(ADDON.getSetting('action')) + LANGOFFSET
-        notifyLog('Sleepy Watchdog set mode to: %s for %s' % (mode, _userIdleTime))
-        notifyUser(LOC(32129) % (LOC(_action), _userIdleTime))
+        if mode == 'USER':
+            ADDON.setSetting('userIdleTime', str(int((re.match('\d+', _userIdleTime).group())) * 60))
+            _action = int(ADDON.getSetting('action')) + LANGOFFSET
+            notifyLog('Sleepy Watchdog set mode to: %s for %s' % (mode, _userIdleTime))
+            notifyUser(LOC(32129) % (LOC(_action), _userIdleTime))
+        else:
+            notifyLog('Sleepy Watchdog set mode to: %s' % (mode))
+            notifyUser(LOC(32211))
     else:
-        notifyUser(LOC(32139), icon=ICON_ERROR)
+        notifyUser(LOC(32139))
